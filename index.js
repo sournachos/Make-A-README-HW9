3e08f714c013f99a1e90c18fa51e387802960918
sournachos <joserivera18100@gmail.com>
2021-04-16 00:46:07 -0500

    Small edit

--- index.js
+++ index.js
@@ -2,68 +2,28 @@ const fs = require('fs'); //Standard Node library to read and write files.
 const inquirer = require('inquirer'); //Node library to personalize inputs
 const markRead = require('./utils/generateMarkdown'); // Path to Javascript file with licensing information
 
-// Creating an array 
-// const questions = ['Enter README description:', ''];
-function init() { 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'Enter Project Title:',
-            name: 'title',
-        },
-        {
-            type: 'input',
-            message: 'Enter README Description:',
-            name: 'desc',
-        },
-        {
-            type: 'input',
-            message: 'Enter Installation Instructions:',
-            name: 'inst',
-        },
-        {
-            type: 'input',
-            message: 'Enter Usage Information:',
-            name: 'use',
-        },
-        {
-            type: 'input',
-            message: 'Enter Contribution Guidelines:',
-            name: 'cont',
-        },
-        {
-            type: 'input',
-            message: 'Enter Test Instructions:',
-            name: 'test',
-        },
-        {
-            type: 'input',
-            message: 'Enter Github Username:',
-            name: 'gituser',
-        },
-        {
-            type: 'list',
-            message: 'Choose License(s) For Your Project:',
-            choices: ['MIT', 'Apache','BSD 2-clause', 'BSD 3-clause'],
-            name: 'lic',
-        },
-        {
-            type: 'input',
-            message: 'Enter Email Users Can Send Questions To:',
-            name: 'email',
-        },
+// Prompts questions to users
+function init() {
+    inquirer.prompt([
+        { type: 'input', name: 'title', message: 'Enter Project Title:' },
+        { type: 'input', name: 'desc', message: 'Enter README Description:' },
+        { type: 'input', name: 'inst', message: 'Enter Installation Instructions:' },
+        { type: 'input', name: 'use', message: 'Enter Usage Information:' },
+        { type: 'input', name: 'cont', message: 'Enter Contribution Guidelines:' },
+        { type: 'input', name: 'test', message: 'Enter Test Instructions:' },
+        { type: 'input', name: 'gituser', message: 'Enter Github Username:' },
+        { type: 'input', name: 'email', message: 'Enter Email Users Can Send Questions To:' },
+        { type: 'list', name: 'lic', message: 'Choose License(s) For Your Project:', choices: ['MIT', 'Apache', 'BSD 2-clause', 'BSD 3-clause'] }
     ])
-    .then((response)=>{
-        function writeToFile(fileName, data) {
-            fs.writeFile(fileName, data, (err) =>
-            err ? console.error(err) : console.log('Success!')
-          );
-        }
-        writeToFile('README.md', markRead.generateMarkdown(response), (err)=>{
-            err ?console.error(err) : console.log('README successfully created')
+        .then((res) => {
+            function writeToFile(fileName, res) {
+                fs.writeFile(fileName, res, (err) =>
+                    err ? console.error(err) : console.log('README successfully created')
+                );
+            }
+            writeToFile('README.md', markRead.generateMarkdown(res))
         })
-    }).catch((err)=>{err ?console.error(err) : console.log('README successfully created')})
+        .catch((err) => console.error(err))
 }
 
 init();
